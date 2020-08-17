8e7ae06eb8664314200728bb64c63625d23e6c7a
veseli <veseli193kg@gmail.com>
2020-08-17 16:08:32 +0200

    Added following essential files to the repo

---
+++ .bashrc
@@ -0,0 +1,2 @@
+export JAVA_HOME=/usr
+export PATH=$JAVA_HOME/bin:$PATH
---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { name as appName } from './app.json'
+import App from './App';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
